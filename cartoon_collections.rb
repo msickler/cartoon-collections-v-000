3bbaffff1ec3bc8367482b7fa4aa1dda0c336bd4
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-29 00:58:25 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,15 @@ def summon_captain_planet(planeteer_calls)
  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.each do |word| 
+    if word.length > 4 
+      true
+    else 
+      false 
+    end
+  end
+
 end
 
 def find_the_cheese# code an argument here
