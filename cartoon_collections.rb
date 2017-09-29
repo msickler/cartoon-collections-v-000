06feb0ce6dd4ec0d4a0fd66342de8831cfd38760
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-29 00:53:40 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,8 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
- planeteer_calls.collect do |planeteer| planeteer.capitalize
-   puts "#{planeteer}!"
+ planeteer_calls.collect do |planeteer| planeteer.capitalize + "!"
+  
  end
 end
 
