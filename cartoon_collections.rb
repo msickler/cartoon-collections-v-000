351cf92bef4fa00a597500c40a0df0c8979ec0ac
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-29 01:09:33 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,6 +18,7 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(array)
- array.find {|cheese| cheese_types.include?(cheese)}
+
   cheese_types = ["cheddar", "gouda", "camembert"]
+   array.find {|cheese| cheese_types.include?(cheese)}
 end
