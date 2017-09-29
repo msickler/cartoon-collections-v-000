ffa637e8ba30e6be2d5c550cfadb03e4c9201ae9
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-29 00:45:14 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index do |dwarf, index| 
+  dwarves.each_with_index do |dwarf, index|
    number = index + 1
     puts "#{number} #{dwarf}"
   end
